b06db598f12a00b99d6648e309186838119e3a42
Dragos <edragos33@gmail.com>
2025-01-31 09:36:39 +0100

    second commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.fizzBuzz(3);
+        fb.fizzBuzz(5);
+        fb.fizzBuzz(15);
+    }
+}
